70dff229a38738d025be04c8903a11ccc49bd087
Shu Yamani <mail@shuymn.me>
2021-10-09 02:26:10 +0900

    feat: end chapter 2

---
+++ 2_standard-patterns/filesystem.zig
@@ -0,0 +1,48 @@
+const std = @import("std");
+const expect = std.testing.expect;
+const eql = std.mem.eql;
+
+test "createFile, write, seekTo, read" {
+    const file = try std.fs.cwd().createFile("junk_file.txt", .{ .read = true });
+    defer file.close();
+
+    const bytes_written = try file.writeAll("Hello File!");
+    _ = bytes_written;
+
+    var buffer: [100]u8 = undefined;
+    try file.seekTo(0);
+    const bytes_read = try file.readAll(&buffer);
+
+    try expect(eql(u8, buffer[0..bytes_read], "Hello File!"));
+}
+
+test "file stat" {
+    const file = try std.fs.cwd().createFile("junk_file2.txt", .{});
+    defer file.close();
+    const stat = try file.stat();
+    try expect(stat.size == 0);
+    try expect(stat.kind == .File);
+    try expect(stat.ctime <= std.time.nanoTimestamp());
+    try expect(stat.mtime <= std.time.nanoTimestamp());
+    try expect(stat.atime <= std.time.nanoTimestamp());
+}
+
+test "make dir" {
+    try std.fs.cwd().makeDir("test-tmp");
+    const dir = try std.fs.cwd().openDir("test-tmp", .{ .iterate = true });
+    defer {
+        std.fs.cwd().deleteTree("test-tmp") catch unreachable;
+    }
+
+    _ = try dir.createFile("x", .{});
+    _ = try dir.createFile("y", .{});
+    _ = try dir.createFile("z", .{});
+
+    var file_count: usize = 0;
+    var iter = dir.iterate();
+    while (try iter.next()) |entry| {
+        if (entry.kind == .File) file_count += 1;
+    }
+
+    try expect(file_count == 3);
+}
---
+++ 2_standard-patterns/readers_and_writers.zig
@@ -0,0 +1,83 @@
+const std = @import("std");
+const expect = std.testing.expect;
+const eql = std.mem.eql;
+const ArrayList = std.ArrayList;
+const test_allocator = std.testing.allocator;
+
+test "io writer usage" {
+    var list = ArrayList(u8).init(test_allocator);
+    defer list.deinit();
+    const bytes_written = try list.writer().write("Hello World!");
+    try expect(bytes_written == 12);
+    try expect(eql(u8, list.items, "Hello World!"));
+}
+
+test "io reader usage" {
+    const message = "Hello File!";
+
+    const file = try std.fs.cwd().createFile("junk_file2.txt", .{ .read = true });
+    defer file.close();
+
+    try file.writeAll(message);
+    try file.seekTo(0);
+
+    const contents = try file.reader().readAllAlloc(test_allocator, message.len);
+    defer test_allocator.free(contents);
+
+    try expect(eql(u8, contents, message));
+}
+
+fn nextLine(reader: anytype, buffer: []u8) !?[]const u8 {
+    var line = (try reader.readUntilDelimiterOrEof(buffer, '\n')) orelse return null;
+    // trim annoying windows-only carriage return character
+    if (std.builtin.os.tag == .windows) {
+        line = std.mem.trimRight(u8, line, '\r');
+    }
+    return line;
+}
+
+test "read until next line" {
+    const stdout = std.io.getStdOut();
+    const stdin = std.io.getStdIn();
+
+    try stdout.writeAll(
+        \\ Enter your name:
+    );
+
+    var buffer: [100]u8 = undefined;
+    const input = (try nextLine(stdin.reader(), &buffer)).?;
+    try stdout.writer().print("Your name is : \"{s}\"\n", .{input});
+}
+
+// Don't create a type like this
+// Use an ArrayList with a Fixed Buffer Allocator
+const MyByteList = struct {
+    data: [100]u8 = undefined,
+    items: []u8 = &[_]u8{},
+
+    const Writer = std.io.Writer(
+        *MyByteList,
+        error{EndOfBuffer},
+        appendWrite,
+    );
+
+    fn appendWrite(self: *MyByteList, data: []const u8) error{EndOfBuffer}!usize {
+        if (self.items.len + data.len > self.data.len) {
+            return error.EndOfBuffer;
+        }
+        std.mem.copy(u8, self.data[self.items.len..], data);
+        self.items = self.data[0 .. self.items.len + data.len];
+        return data.len;
+    }
+
+    fn writer(self: *MyByteList) Writer {
+        return .{ .context = self };
+    }
+};
+
+test "custom writer" {
+    var bytes = MyByteList{};
+    _ = try bytes.writer().write("Hello");
+    _ = try bytes.writer().write(" Writer!");
+    try expect(eql(u8, bytes.items, "Hello Writer!"));
+}
